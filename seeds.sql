80fb5bbdc6e5aac844c1abba5c50054f4b7fb57b
Kathy <kzhang193@gmail.com>
2018-12-16 00:27:20 -0600

    Create seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("socks", "clothing", 6.50, 572),
+("earrings", "jewelry", 200.00, 153),
+("nintendo switch", "electronics", 299.99, 75),
+("coffee maker", "kitchen", 40.79, 300),
+("oculus rift", "electronics", 350.00, 99),
+("leggings", "clothing", 30.65, 422),
+("toaster oven", "kitchen", 15.50, 56),
+("beanie", "clothing", 19.99, 345),
+("golf clubs", "sports", 160.78, 34),
+("yoga mat", "fitness", 39.99, 789);
+
+SELECT * FROM bamazon.products;
+
+
+
